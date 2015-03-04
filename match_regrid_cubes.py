5e45ba9a68ff929c5d3227ee26b74b1a9985bc3a
e-koch <koch.eric.w@gmail.com>
2015-03-04 16:05:56 -0700

    Working method. Routines to make new header and restore the shape by channel.

--- match_regrid_cubes.py
+++ match_regrid_cubes.py
@@ -13,7 +13,7 @@ from astropy.wcs import WCS
 
 
 def match_regrid(filename1, filename2, reappend_dim=True,
-                 degrade_factor=(1, 2, 2),
+                 degrade_factor=(1, 1, 8, 8), restore_dim=True,
                  remove_hist=True, save_output=False, save_name='new_img'):
     '''
     Input two fits filenames. The output will be the projection of file 1
@@ -23,13 +23,14 @@ def match_regrid(filename1, filename2, reappend_dim=True,
     fits1 = fits.open(filename1)
     fits2 = fits.open(filename2)
 
-    hdr1 = fits1[0].header.copy()
-    hdr2 = fits2[0].header.copy()
+    hdr1 = fits1[0].header
+    hdr2 = fits2[0].header
 
-    hdr2["CUNIT4"] = 'km/s    '
-    hdr2["CRVAL4"] = -48.1391
-    hdr2["CTYPE4"] = 'VELO-LSR'
-    hdr2["CDELT4"] = -1.288141
+    # Check that they are in the same velocity frame.
+    # There currently isn't a non-CASA python method to do this, so the task
+    # just raises an error.
+    if hdr1['SPECSYS'] != hdr2['SPECSYS']:
+        raise ValueError('Data are in different spectral reference frames.')
 
     if remove_hist:
         # Remove the huge CASA history
@@ -49,24 +50,92 @@ def match_regrid(filename1, filename2, reappend_dim=True,
     shape2 = fits2[0].data.shape
 
     # Change shape to match degradation
-    shape2 = tuple(i/j for i, j in zip(shape2, degrade_factor))
+    deg_shape2 = tuple(i/j for i, j in zip(shape2, degrade_factor))
 
     fits2.close()
+
+    new_hdr2 = new_wcs.to_header()
+
+    for i, s in enumerate(deg_shape2[::-1]):
+        new_hdr2['NAXIS'+str(i+1)] = s
+    new_hdr2['NAXIS'] = len(deg_shape2)
 
     # Do the matching
     if len(shape1) == 2:
         regrid_img = ft.hcongrid.hcongrid(fits1[0].data, fits1[0].header, hdr2)
     else:
-        # regrid_img = ft.regrid_cube(fits1[0].data, fits1[0].header, hdr2, specaxes=(3, 3))
-        regrid_img = reproject(fits1[0], hdr2, shape_out=shape2)[0]
+        regrid_img = ft.regrid_cube(fits1[0].data, hdr1, new_hdr2,
+                                    specaxes=(2, 2))
+        regrid_img = regrid_img.reshape((1,)+regrid_img.shape)
+
+    if restore_dim:
+        regrid_hdr = _regrid_header(hdr1, hdr2)
+        regrid_img = _restore_shape(regrid_img, degrade_factor)
+    else:
+        regrid_hdr = _regrid_header(hdr1, new_hdr2)
 
     if save_output:
-        hdu = fits.PrimaryHDU(regrid_img, header=hdr2)
+        hdu = fits.PrimaryHDU(regrid_img, header=regrid_hdr)
         hdu.writeto(save_name+".fits")
 
     else:
-        return fits.PrimaryHDU(regrid_img, header=hdr2)
+        return fits.PrimaryHDU(regrid_img, header=regrid_hdr)
 
+
+def _restore_shape(cube, zoom_factor, vel_axis=1, verbose=True):
+    '''
+    Interpolates the cube by channel to the given shape. Assumes
+    velocity dimension has not been degraded.
+    '''
+
+    naxis = len(cube.shape)
+
+    vel_shape = cube.shape[vel_axis]
+
+    from scipy.ndimage import zoom
+
+    vel_slice = [slice(None)] * naxis
+
+    for v in np.arange(vel_shape):
+        print 'Channel %s/%s' % (v+1, vel_shape)
+        vel_slice[vel_axis] = slice(v, v+1)
+
+        plane = cube[vel_slice]
+
+        bad_pix = ~np.isfinite(plane)
+
+        plane[bad_pix] = 0
+
+        zoom_plane = zoom(plane, zoom_factor)
+        zoom_bad_pix = zoom(bad_pix, zoom_factor)
+
+        zoom_plane[zoom_bad_pix] = np.NaN
+
+        if v == 0:
+            full_cube = zoom_plane
+        else:
+            full_cube = np.dstack((full_cube, zoom_plane))
+
+    return full_cube.T
+
+
+def _regrid_header(header1, header2):
+    '''
+    Make a header for the regridded image.
+    '''
+
+    new_hdr = header1.copy()
+
+    naxes = header1['NAXIS']
+
+    for i in range(1, naxes+1):
+        new_hdr['CRPIX'+str(i)] = header2['CRPIX'+str(i)]
+        new_hdr['CTYPE'+str(i)] = header2['CTYPE'+str(i)]
+        new_hdr['CRVAL'+str(i)] = header2['CRVAL'+str(i)]
+        new_hdr['CDELT'+str(i)] = header2['CDELT'+str(i)]
+        new_hdr['NAXIS'+str(i)] = header2['NAXIS'+str(i)]
+
+    return new_hdr
 
 if __name__ == '__main__':
 
